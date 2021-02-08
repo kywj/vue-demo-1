3aa54e9faf66e81d97cf3f8149f723b111cc5840
kongyi <1269428345@qq.com>
2021-02-08 11:45:46 +0800

    el、template、methods、components

--- src/main.js
+++ src/main.js
@@ -1,9 +1,38 @@
-import Vue from 'vue'
+const Vue = window.Vue
+Vue.config.productionTip = false
+
 import Demo from './demo.vue'
 
 new Vue({
-  el: '#app',
-  render(h){
-    return h(Demo)
+  components: {
+    Demo
   },
-})
+  // el: '#jerry',
+  //  template: `
+  //  <div>hi</div>
+  // `,
+ data(){
+  return {
+    n: 0,
+    array: [1,2,3,4,5,6,7,8]
+  } 
+ },
+ template: `
+  <div class="red">
+  {{n}}
+  <button @click="add">+1</button>
+  <Demo />
+  <hr>
+  {{filter()}}
+  </div>
+ `,
+ methods: {
+   add(){
+     this.n += 1
+   },
+   filter(){
+     console.log('执行了 filter 函数')
+    return this.array.filter(i => i % 2 === 0)
+   }
+ }
+}).$mount('#jerry')
